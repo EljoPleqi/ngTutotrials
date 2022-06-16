d10a80647ee73090912ee1cc6e46c58a6cbd11ba
eljopleqi <eljopleqi@gmail.com>
2022-06-16 12:46:32 +0200

    create navigation and added the functionality for the recipe-item component

--- 02/src/app/app.component.ts
+++ 02/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  activeView: string = 'Recipes';
   title = '02';
+
+  switch(selected: string) {
+    this.activeView = selected;
+  }
 }
